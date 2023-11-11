eba69448424ce27f5bdf15771cbff8bb171f3bfa
DIN88888888 <150512656+DIN88888888@users.noreply.github.com>
2023-11-12 01:33:04 +0700

    1.10 dz

---
+++ 1_10_dz/1_10_dz/1_10_dz/1_10_dz.cpp
@@ -0,0 +1,32 @@
+﻿// 1_10_dz.cpp 
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    bool init = true;
+
+    while (init)
+    {
+        long long a, b;
+        cout << "enter numbers like \"1 2\":" << endl;
+        cin >> a >> b;//+  -  *  /  &  |  ^
+        cout << "a + b = " << a + b << endl;
+        cout << "a - b = " << a - b << endl;
+        cout << "a * b = " << a * b << endl;
+        cout << "a / b = " <<  ((long double)a / (long double)b) << endl;
+        cout << "a & b = " << (a & b) << endl;
+        cout << "a | b = " << (a | b) << endl;
+        cout << "a ^ b = " << (a ^ b) << endl;
+
+        cout << "again?(y/n)" << endl<< ">";        
+
+        char q;
+        cin >> q;
+
+        init = q=='y';
+    }
+}
+
